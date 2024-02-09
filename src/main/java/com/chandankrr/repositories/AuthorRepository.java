8149118ea90a2776214b4551c559cd76dcbdf5da
Chandan Kumar <chandankrr.91@gmail.com>
2024-02-09 16:09:53 +0530

    test: integration test for save and read data for entities

--- src/main/java/com/chandankrr/repositories/AuthorRepository.java
+++ src/main/java/com/chandankrr/repositories/AuthorRepository.java
@@ -1,4 +1,10 @@
 package com.chandankrr.repositories;
 
-public interface AuthorRepository {
+import com.chandankrr.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
 }
--- src/main/java/com/chandankrr/repositories/BookRepository.java
+++ src/main/java/com/chandankrr/repositories/BookRepository.java
@@ -1,4 +1,10 @@
 package com.chandankrr.repositories;
 
-public interface BookRepository {
+import com.chandankrr.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, String> {
+
 }
