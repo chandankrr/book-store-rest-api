5b47ccdcc352baa8b45bf514ae84bb975de7a97c
Chandan Kumar <chandankrr.91@gmail.com>
2024-02-09 17:06:18 +0530

    learn about HQL (Hibernate Query Language)

--- src/main/java/com/chandankrr/repositories/AuthorRepository.java
+++ src/main/java/com/chandankrr/repositories/AuthorRepository.java
@@ -1,6 +1,7 @@
 package com.chandankrr.repositories;
 
 import com.chandankrr.domain.Author;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Repository;
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 
     Iterable<Author> ageLessThan(int age);
+
+    @Query("SELECT a FROM Author a WHERE age > ?1")
+    Iterable<Author> findAuthorsWithAgeGreaterThan(int age);
+
 }
